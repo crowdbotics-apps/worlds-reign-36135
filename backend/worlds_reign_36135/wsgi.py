041d08d75a3d967a1935652279415292f81dd908
Crowdbotics <team@crowdbotics.com>
2022-07-11 10:23:16 +0000

    Initial commit.

---
+++ backend/worlds_reign_36135/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for worlds_reign_36135 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'worlds_reign_36135.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "worlds_reign_36135",
+  url: "https://worlds_reign_36135.botics.co",
+  api: "https://worlds_reign_36135.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
